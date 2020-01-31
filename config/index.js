f086cfc686b66af099949922809d335a330cb6ec
danielclough <danielclough@gmail.com>
2020-01-31 01:33:44 -0800

    fix

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'SilkMint', // Navigation and Site Title
+  siteTitleAlt: 'SilkMint: Smooth - Cool - Refreshing', // Alternative Site title for SEO
+  siteTitleShort: 'SilkMint', // short_name for manifest
+  siteHeadline: 'Smooth - Cool - Refreshing', // Headline for schema.org JSONLD
+  siteUrl: 'https://silkmint.daniec.us', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteLogo: '/logos/logo.jpg', // Used for SEO and manifest
+  siteDescription: 'Modeling portfolio for SilkMint',
+  author: 'DanielC.us', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@jodie', // Twitter Username
-  ogSiteName: 'jodie', // Facebook Site Name
+  userTwitter: '@silkmint', // Twitter Username
+  ogSiteName: 'SilkMint', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
   googleAnalyticsID: 'UA-XXXXXX-X',
 

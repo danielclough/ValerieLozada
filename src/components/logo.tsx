203fd89453dc0f7e99d4ec42103cbbe74fd1bcca
danielclough <danielclough@gmail.com>
2020-02-01 14:50:58 -0800

    update

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
 const Logo = () => (
-  <svg viewBox="0 0 493.19 493.19">
+  <image>
     <title>Valerie Lozada</title>
-    <path
-       d="M 0,113.33334 V 0 H 107.33334 214.66667 V 113.33334 226.66667 H 107.33334 0 Z" />
-  </svg>
+  </image>
 )
 
 export default Logo
